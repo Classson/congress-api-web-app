61b7c89ad342c8b33f8c6609705ea6afb0135194
Alex <Jillbk22@gmail.com>
2018-08-15 01:31:13 -0400

    adjusted formatting in script

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
 //geolocation to get lat and long
 if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function (position) {
+  navigator.geolocation.getCurrentPosition(function (position) 
+    {
       let currentLat = position.coords.latitude;
       let currentLong = position.coords.longitude;
       
@@ -10,75 +11,79 @@ if (navigator.geolocation) {
       locationGet = new XMLHttpRequest();
       locationGet.open('GET', locationURL, true);
       locationGet.send();
-      locationGet.onload = function() {
-        locationInfo = JSON.parse(locationGet.responseText);
+      locationGet.onload = function() 
+        {
+            locationInfo = JSON.parse(locationGet.responseText);
 
-        state = locationInfo.results[0].address_components.state;
-        city = locationInfo.results[0].address_components.city;
-        console.log(state);
-        console.log(city);  
-      
-        document.getElementById('city').innerHTML = city;
-      }
+            state = locationInfo.results[0].address_components.state;
+            city = locationInfo.results[0].address_components.city;
+            console.log(state);
+            console.log(city);  
+
+            document.getElementById('city').innerHTML = city;
+        }
       
     // get info from sunrise-sunset api  
     let sunUrl = "https://api.sunrise-sunset.org/json?lat=" + currentLat + "&lng=" + currentLong + "&date=today";
       
-      function getSunInfo() {
+      function getSunInfo() 
+        {
         dataObject = new XMLHttpRequest();
         dataObject.open('GET', sunUrl, true);
 
         dataObject.send();
-        dataObject.onload = function() {
+        dataObject.onload = function() 
+            {
 
-          sunInfo = JSON.parse(dataObject.responseText);
-          console.log(sunInfo);
+            sunInfo = JSON.parse(dataObject.responseText);
+            console.log(sunInfo);
         
-        sunSet = sunInfo.results.sunset; 
-        sunRise = sunInfo.results.sunrise;
+            sunSet = sunInfo.results.sunset; 
+            sunRise = sunInfo.results.sunrise;
             
         //get local date and calculate offset
-        let nowDate = new Date();
-        offset = nowDate.getTimezoneOffset();
-        offsetHours = offset / 60;
+            let nowDate = new Date();
+            offset = nowDate.getTimezoneOffset();
+            offsetHours = offset / 60;
 
         //regular expression to find hours in sunrise and set
-        let hourRegex = /[0-9]+:/;
-        let afterRegex = /:[0-9a-z: ]*/i;
+            let hourRegex = /[0-9]+:/;
+            let afterRegex = /:[0-9a-z: ]*/i;
 
             
         //use regExs to find hours in sunRise    
-        let hourRiseObj = sunRise.match(hourRegex);
-        let hourRise = hourRiseObj[0];
-        let intRise = parseInt(hourRise, 10);
+            let hourRiseObj = sunRise.match(hourRegex);
+            let hourRise = hourRiseObj[0];
+            let intRise = parseInt(hourRise, 10);
             
-        let afterRiseHourObj = sunRise.match(afterRegex);
+            let afterRiseHourObj = sunRise.match(afterRegex);
 
             
         //use regExs to find hours in sunSet
-        let hourSetObj = sunSet.match(hourRegex);
-        let hourSet = hourSetObj[0];
-        let intSet = parseInt(hourSet, 10);
+            let hourSetObj = sunSet.match(hourRegex);
+            let hourSet = hourSetObj[0];
+            let intSet = parseInt(hourSet, 10);
             
-        let afterSetHourObj = sunSet.match(afterRegex);
+            let afterSetHourObj = sunSet.match(afterRegex);
         
 
         //substracting offset from hours
-        let convRiseHour = intRise - offsetHours;
-        let convSetHour = intSet - offsetHours;
+            let convRiseHour = intRise - offsetHours;
+            let convSetHour = intSet - offsetHours;
 
         //adding the converted hours back to the minutes:seconds
-        let convRise = convRiseHour + afterRiseHourObj[0];
-        let convSet = convSetHour + afterSetHourObj[0];
+            let convRise = convRiseHour + afterRiseHourObj[0];
+            let convSet = convSetHour + afterSetHourObj[0];
             
         // insert converted times into html  
-        document.getElementById('sunRiseTime').innerHTML = convRise;
-        document.getElementById('sunSetTime').innerHTML = convSet;
+            document.getElementById('sunRiseTime').innerHTML = convRise;
+            document.getElementById('sunSetTime').innerHTML = convSet;
         }
       }
       
-      getSunInfo();
-    })}
+    getSunInfo();
+})
+}
 
 
 
